46115502dd1599f8587c958f28b4af58c84f5eb4
J700070 <juanhs17@protonmail.com>
2021-08-28 16:33:10 +0200

    Added financial data & fixed db structure

--- dataScrapper.js
+++ dataScrapper.js
@@ -11,21 +11,57 @@ export async function fetchMorningstar(stock) {
 
     await page.goto(stockUrl, { waitUntil: 'networkidle2' });
 
-    let data = await page.evaluate(() => {
+    if (page.url() !== stockUrl) {
+        console.log("Este stock no existe en MorningStar");
+        return null;
+    }
 
+    let data = await page.evaluate(() => {
+        // General Info
         let ticker = document.querySelector('div[class="r_title"] > span[class="gry"]').innerText.substring(1);
         let name = document.querySelector('div[class="r_title"] > h1').innerText;
+
+        // Fechas
+        let dates = [];
+
+        // MorningStar financials
         let revenue = [];
         let operatingIncome = [];
         let netIncome = [];
-        let shares = [];
         let eps = [];
-        let fcf = [];
-        let fcfPerShare = [];
+        let dividends = [];
+        let payoutRatio = [];
+        let shares = [];
+        let bookValue = [];
+        let bookValuePerShare = [];
+        let operatingCashFlow = [];
+        let capSpending = [];
+        let freeCashFlow = [];
+        let freeCashFlowPerShare = [];
+        let workingCapital = [];
+
+
+        // MorningStar profitability -> Margins % of Sales
+        let cogs = [];
+        let grossMargin = [];
+        let sgAndA = [];
+        let rAndD = [];
+        let other = [];
         let operatingMargin = [];
+        let netIntIncAndOther = [];
+        let ebtMargin = [];
         let netMargin = [];
+
+        // MorningStar profitability -> Profitability
+        let taxRate = [];
+        let assetTurnoverAverage = [];
+        let roa = [];
+        let financialLeverage = [];
         let roe = [];
         let roic = [];
+        let interestCoverage = [];
+
+        // MorningStar growth
         let shareGrowth = [];
         let revenueGrowth = [];
         let operatingIncomeGrowth = [];
@@ -33,57 +69,71 @@ export async function fetchMorningstar(stock) {
         let epsGrowth = [];
 
         for (let i = 0; i < 11; i++) {
-            let statementRevenue = 'td[headers="YX i0"]';
-            let statementOperatingIncome = 'td[headers="YX i2"]';
-            let statementNetIncome = 'td[headers="YX i4"]';
-            let statementShares = 'td[headers="YX i7"]';
-            let statementFcf = 'td[headers="YX i11"]';
-            let statementROE = 'td[headers="pr-pro-YX pr-profit i26"]';
-            let statementROIC = 'td[headers="pr-pro-YX pr-profit i27"]';
+            // Fechas
+            let datesCell = 'th[id="YX"]';
 
-            statementRevenue = statementRevenue.replace("X", i);
-            statementOperatingIncome = statementOperatingIncome.replace("X", i);
-            statementNetIncome = statementNetIncome.replace("X", i);
-            statementShares = statementShares.replace("X", i);
-            statementFcf = statementFcf.replace("X", i);
-            statementROE = statementROE.replace("X", i);
-            statementROIC = statementROIC.replace("X", i);
+            // MorningStar financials
+            let revenueCell = 'td[headers="YX i0"]';
+            let operatingIncomeCell = 'td[headers="YX i1"]';
+            let netIncomeCell = 'td[headers="YX i2"]';
+            let epsCell = 'td[headers="YX i3"]';
+            let dividendsCell = 'td[headers="YX i4"]';
+            let payoutRatioCell = 'td[headers="YX i5"]';
+            let sharesCell = 'td[headers="YX i6"]';
+            let bookValueCell = 'td[headers="YX i7"]';
+            let bookValuePerShareCell = 'td[headers="YX i8"]';
+            let operatingCashFlowCell = 'td[headers="YX i9"]';
+            let capSpendingCell = 'td[headers="YX i10"]';
+            let freeCashFlowCell = 'td[headers="YX i11"]';
+            let freeCashFlowPerShareCell = 'td[headers="YX i90"]';
+            let workingCapitalCell = 'td[headers="YX i80"]';
 
-            revenue[i] = parseFloat(document.querySelector(statementRevenue).innerText.replace(",", ""));
-            operatingIncome[i] = parseFloat(document.querySelector(statementOperatingIncome).innerText.replace(",", ""));
-            netIncome[i] = parseFloat(document.querySelector(statementNetIncome).innerText.replace(",", ""));
-            shares[i] = parseFloat(document.querySelector(statementShares).innerText.replace(",", ""));
-            eps[i] = netIncome[i] / shares[i];
-            fcf[i] = parseFloat(document.querySelector(statementFcf).innerText.replace(",", ""));
-            fcfPerShare[i] = fcf[i] / shares[i];
-            operatingMargin[i] = operatingIncome[i] / shares[i];
-            netMargin[i] = netIncome[i] / shares[i];
-            roe[i] = parseFloat(document.querySelector(statementROE).innerText.replace(",", ""));
-            roic[i] = parseFloat(document.querySelector(statementROIC).innerText.replace(",", ""));
-            if (i == 0) {
-                shareGrowth[i] = 999;
-                revenueGrowth[i] = 999;
-                operatingIncomeGrowth[i] = 999;
-                netIncomeGrowth[i] = 999;
-                epsGrowth[i] = 999;
-            } else {
-                shareGrowth[i] = (shares[i] / shares[i - 1] - 1) * 100;
-                revenueGrowth[i] = (revenue[i] / revenue[i - 1] - 1) * 100;
-                operatingIncomeGrowth[i] = (operatingIncome[i] / operatingIncome[i - 1] - 1) * 100;
-                netIncomeGrowth[i] = (netIncome[i] / netIncome[i - 1] - 1) * 100;
-                epsGrowth[i] = (eps[i] / eps[i - 1] - 1) * 100;
+            datesCell = datesCell.replace("X", i);
+            revenueCell = revenueCell.replace("X", i);
+            operatingIncomeCell = operatingIncomeCell.replace("X", i);
+            netIncomeCell = netIncomeCell.replace("X", i);
+            epsCell = epsCell.replace("X", i);
+            dividendsCell = dividendsCell.replace("X", i);
+            payoutRatioCell = payoutRatioCell.replace("X", i);
+            sharesCell = sharesCell.replace("X", i);
+            bookValueCell = bookValueCell.replace("X", i);
+            bookValuePerShareCell = bookValuePerShareCell.replace("X", i);
+            operatingCashFlowCell = operatingCashFlowCell.replace("X", i);
+            capSpendingCell = capSpendingCell.replace("X", i);
+            freeCashFlowCell = freeCashFlowCell.replace("X", i);
+            freeCashFlowPerShareCell = freeCashFlowPerShareCell.replace("X", i);
+            workingCapitalCell = workingCapitalCell.replace("X", i);
+
+            try {
+                dates[i] = document.querySelector(datesCell).innerText;
+                revenue[i] = document.querySelector(revenueCell).innerText.replace(",", "");
+                operatingIncome[i] = document.querySelector(operatingIncomeCell).innerText.replace(",", "");
+                netIncome[i] = document.querySelector(netIncomeCell).innerText.replace(",", "");
+                eps[i] = document.querySelector(epsCell).innerText.replace(",", "");
+                dividends[i] = document.querySelector(dividendsCell).innerText.replace(",", "");
+                payoutRatio[i] = document.querySelector(payoutRatioCell).innerText.replace(",", "");
+                shares[i] = document.querySelector(sharesCell).innerText.replace(",", "");
+                bookValue[i] = document.querySelector(bookValueCell).innerText.replace(",", "");
+                bookValuePerShare[i] = document.querySelector(bookValuePerShareCell).innerText.replace(",", "");
+                operatingCashFlow[i] = document.querySelector(operatingCashFlowCell).innerText.replace(",", "");
+                capSpending[i] = document.querySelector(capSpendingCell).innerText.replace(",", "");
+                freeCashFlow[i] = document.querySelector(freeCashFlowCell).innerText.replace(",", "");
+                freeCashFlowPerShare[i] = document.querySelector(freeCashFlowPerShareCell).innerText.replace(",", "");
+                workingCapital[i] = document.querySelector(workingCapitalCell).innerText.replace(",", "");
+            } catch (error) {
+
+                return null
             }
 
 
         }
 
         return {
-            name, ticker, revenue, operatingIncome, netIncome, shares, eps, fcf, fcfPerShare, operatingMargin, netMargin, roe, roic, shareGrowth, revenueGrowth, operatingIncomeGrowth, netIncomeGrowth, epsGrowth
+            name, ticker, dates, revenue, operatingIncome, netIncome, eps, dividends, payoutRatio, shares, bookValue, bookValuePerShare, operatingCashFlow, capSpending, freeCashFlow, freeCashFlowPerShare, workingCapital
         };
 
     })
 
-    console.log(data);
 
     await browser.close();
 
@@ -98,7 +148,7 @@ export async function fetchStockTickerList() {
     let browser = await puppeteer.launch();
     let page = await browser.newPage();
     await page.goto(stockUrl, { waitUntil: 'networkidle2' });
-
+    await page.select('select[name="perpage"]', '10000');
     let data = await page.evaluate(() => {
         let stockList = [];
         let j = 0;
@@ -106,10 +156,10 @@ export async function fetchStockTickerList() {
             stockList[j++] = document.querySelectorAll('table > tbody > tr > td')[i].innerText;
         }
 
+
         return stockList;
     })
 
-    console.log(data);
 
     await browser.close();
 
